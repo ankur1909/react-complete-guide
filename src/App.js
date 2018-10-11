28f1a2020ad4a3e2298a1412acd104a2a9a973c5
ankur1909 <ankur.2323@gmail.com>
2018-10-12 01:15:47 +0530

    Before running npm Eject for CSS modules...everything works till now

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
   };
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -67,10 +67,20 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
     return (
       <div className="App">
         <h1>Hi,I'm a react app</h1>
+        <p className={classes.join(" ")}>This is really working</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
